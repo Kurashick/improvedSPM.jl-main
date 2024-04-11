a5a39285aafa6118f5e5ca67c52f65ba0785160e
Kurashick <157878377+Kurashick@users.noreply.github.com>
2024-04-11 14:23:10 +0900

    Add files via upload

---
+++ improvedSPM.jl-main/src/SPM.jl
@@ -0,0 +1,11 @@
+module SPM
+
+include("spm_core.jl")
+include("hdr.jl")
+include("btr.jl")
+include("plots.jl")
+
+using .SPMCore  # SPMをincludeすればSPMCore下で定義されたものはSPM.で呼び出せる
+Plots = SPMPlots # conflictを避けるためにSPMPlotsで定義したが長いのでPlotsにalias
+
+end
